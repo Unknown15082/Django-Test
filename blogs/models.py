654107aa5b81ec3154ec5d731db8a69fd08bb7d2
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-28 12:11:56 +0700

    Added database

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length = 256, verbose_name = 'Name')
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length = 256, verbose_name = 'Title')
+    body = models.CharField(max_length = 1024, verbose_name = 'Body')
+    created_time = models.DateTimeField(verbose_name = "Time created")
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}: {self.body}"
