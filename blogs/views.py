75abc911003d3a3ed7250b3add7e3cbb22009d61
Trần Gia Huy <trangiahuy15082006@gmail.com>
2022-02-28 17:55:42 +0700

    Added new views

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello!")
+    latest_posts = Post.objects.order_by('-created_time')[:10]
+    latest_posts_text = [str(p) for p in latest_posts]
+    return HttpResponse(', '.join(latest_posts_text))
+
+def detail(request, post_id):
+    post = Post.objects.get(pk = post_id)
+    post_text = str(post)
+    return HttpResponse(f"Post #{post_id}: {post_text}")
--- src/urls.py
+++ src/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('blogs.urls')),
-    path('index/', include('blogs.urls')),
+    path('blogs/', include('blogs.urls')),
     path('admin/', admin.site.urls),
 ]
